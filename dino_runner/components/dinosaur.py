7b7a7dfc707b5c68e99e303c18586708d19cb693
Unveractus <Jorge.Escobar@fundacion-jala.org>
2023-03-30 19:55:33 -0400

    Clase 1

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,12 @@
+from dino_runner.utils.constants import RUNNING, DUCKING, JUMPING
+class Dinosaur:
+    def __init__(self):
+        
+    def run(self)
+    pass
+
+    def duck(self)
+    pass
+
+    def jump(self)
+    pass
---
+++ dino_runner/components/obstacles/obstacle.py
@@ -0,0 +1,10 @@
+class Obstacle:
+    
+    def __init__(self):
+        pass
+
+    def update (self):
+        pass
+
+    def draw(self):
+        pass
